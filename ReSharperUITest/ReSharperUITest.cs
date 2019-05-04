a50185aa78d2205c5442f0c1875a1afa2451561d
j8kin <emk161278@gmail.com>
2019-05-05 00:45:13 +0300

    replace element.Click() with Actions... since TreeGridDate not allow to use Click() it moves only mouse but not perform action.

--- ReSharperUITest/ReSharperUITest.cs
+++ ReSharperUITest/ReSharperUITest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Remote;
 
 namespace ReSharperUITest
@@ -13,6 +16,7 @@ namespace ReSharperUITest
 
         private WindowsDriver<WindowsElement> _driver;
         private WindowsDriver<WindowsElement> _desktopSession;
+        private int nToggle = 0;
 
         // todo: add start Visual Studio before all TestCases
         // todo: add close Visual Studio after all TestCases
@@ -20,6 +24,7 @@ namespace ReSharperUITest
         [SetUp]
         public void BeforeEachTest()
         {
+            //Console.WriteLine("SetUp");
             // Create a session for Desktop
             var desktopCapabilities = new DesiredCapabilities();
             desktopCapabilities.SetCapability("app", "Root");
@@ -40,9 +45,13 @@ namespace ReSharperUITest
             // Set managed memory usage to not displayed this means that test will always starts on the same point
             if (IsManagedMemoryDisplayed())
             {
+                //Console.WriteLine("Managed is True");
                 ToggleManagedMemoryCheckBox();
             }
-
+            else
+            {
+                //Console.WriteLine("Managed is False");
+            }
         }
 
         [TearDown]
@@ -58,20 +67,8 @@ namespace ReSharperUITest
         }
 
         [Test]
-        public void OpenReSharperOptions()
+        public void SwitchBetweenTabs()
         {
-            if (IsManagedMemoryDisplayed())
-            {
-                ToggleManagedMemoryCheckBox();
-            }
-        }
-
-        /// <summary>
-        /// Open ReSharper Option and toggle "Show managed memory usage in status bar" checkbox
-        /// </summary>
-        private void ToggleManagedMemoryCheckBox()
-        {
-            // Click on ReSharper menu 
             _driver.FindElementByName("ReSharper").Click();
             // Open ReSharper Option Dialog
             //_driver.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]").Click();
@@ -80,7 +77,60 @@ namespace ReSharperUITest
             reSharperMenuItems.First(p => p.Text == "Options…").Click();
             // Find Option Window
             var optionWindow = _driver.FindElementByName("Options");
-            Assert.IsNotNull(optionWindow);
+
+            var gridElem = optionWindow.FindElementByName("Settings");
+            // For unknown reason element.Click() is not working.
+            // It just only mouse move to element position
+            // that is why use Action for this
+            new Actions(_driver).MoveToElement(gridElem).Click().Perform();
+
+            gridElem = optionWindow.FindElementByName("General");
+            // For unknown reason element.Click() is not working.
+            // It just only mouse move to element position
+            // that is why use Action for this
+            new Actions(_driver).MoveToElement(gridElem).Click().Perform();
+        }
+
+        [Test]
+        public void VerifyReSharperManagedMemoryOptionLogicAsync()
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine("Initially Managed Memory is not Displayed");
+            // Initially Managed Memory is not Displayed
+            Assert.False(IsManagedMemoryDisplayed());
+
+            // Toggle Managed Memory Checkbox
+            ToggleManagedMemoryCheckBox();
+
+            // Verify that Managed Memory is Displayed
+            Console.WriteLine("Verify that Managed Memory is Displayed");
+            Assert.True(IsManagedMemoryDisplayed());
+
+            // Toggle Managed Memory Checkbox
+            ToggleManagedMemoryCheckBox();
+
+            // Verify that Managed Memory is not Displayed
+            Console.WriteLine("Verify that Managed Memory is Displayed");
+            Assert.False(IsManagedMemoryDisplayed());
+        }
+
+        /// <summary>
+        /// Open ReSharper Option and toggle "Show managed memory usage in status bar" checkbox
+        /// </summary>
+        private void ToggleManagedMemoryCheckBox()
+        {
+            Console.WriteLine("Toggle Managed Memory Checkbox: " + nToggle);
+            nToggle++;
+            // Click on ReSharper menu 
+            _driver.FindElementByName("ReSharper").Click();
+            // Open ReSharper Option Dialog
+
+            //_driver.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]").Click();
+            // for unknown reason FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]") not able to find "Options…" menu item
+            var reSharperMenuItems = _driver.FindElementsByClassName("MenuItem");
+            reSharperMenuItems.First(p => p.Text == "Options…").Click();
+            // Find Option Window
+            var optionWindow = _driver.FindElementByName("Options");
 
             // EMK it is not necessary to click on "Settings" but 
             //   task description contain the following:
@@ -88,29 +138,26 @@ namespace ReSharperUITest
             //  находить опцию, если открыта любая закладка"
             // So switch to "Settings" to emulate the situation that some other tab is opened
             var settings = optionWindow.FindElementByName("Settings");
-            Assert.IsNotNull(settings);
-            settings.Click();
+            new Actions(_driver).MoveToElement(settings).Click().Perform();
+
 
             // Locate "General" tab
             var general = optionWindow.FindElementByName("General");
-            Assert.IsNotNull(general);
-            general.Click();
+            new Actions(_driver).MoveToElement(general).Click().Perform();
 
             // Locate "Show managed _memory usage in status bar
             var managedMemoryCheckBox = optionWindow.FindElementByName("Show managed _memory usage in status bar");
-            Assert.IsNotNull(managedMemoryCheckBox);
             // toggle "Show managed _memory usage in status bar"
             managedMemoryCheckBox.Click();
 
             // Save & Close "Option" ReSharper Option window
             var saveButton = optionWindow.FindElementByName("Save");
-            Assert.IsNotNull(saveButton);
             saveButton.Click();
         }
 
         private bool IsManagedMemoryDisplayed()
         {
-            return !bool.Parse(_driver.FindElementByClassName("RichTextPresenter").GetAttribute("IsOffscreen"));
+            return Regex.IsMatch(_driver.FindElementByClassName("RichTextPresenter").Text, "\\d+ MB");
         }
     }
 }
