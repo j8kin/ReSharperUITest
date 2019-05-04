34452efeba235a6978d39a11d3ba9d796350e8ff
j8kin <emk161278@gmail.com>
2019-05-04 20:43:18 +0300

    Update test: 1) Create StartUp where setup driver 2) Verify that Managed Memory is not displayed and toggle otherwise 3) Create TearDown where verify that ReSharper Option is not displayed and close it Otherwise

--- ReSharperUITest/ReSharperUITest.cs
+++ ReSharperUITest/ReSharperUITest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Remote;
@@ -13,46 +14,103 @@ namespace ReSharperUITest
         private WindowsDriver<WindowsElement> _driver;
         private WindowsDriver<WindowsElement> _desktopSession;
 
+        // todo: add start Visual Studio before all TestCases
+        // todo: add close Visual Studio after all TestCases
+
         [SetUp]
-        public void TestInit()
+        public void BeforeEachTest()
         {
             // Create a session for Desktop
             var desktopCapabilities = new DesiredCapabilities();
             desktopCapabilities.SetCapability("app", "Root");
-            _desktopSession = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), desktopCapabilities);
+            _desktopSession =
+                new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), desktopCapabilities);
             Assert.IsNotNull(_desktopSession);
             // find Microsoft Visual Studio window todo replace with partially Element Name
             var visualStudioWindow = _desktopSession.FindElementByName("ReSharperUITest - Microsoft Visual Studio ");
-            var visualStudioTopLevelWindowHandle = (int.Parse(visualStudioWindow.GetAttribute("NativeWindowHandle"))).ToString("x");
+            var visualStudioTopLevelWindowHandle =
+                (int.Parse(visualStudioWindow.GetAttribute("NativeWindowHandle"))).ToString("x");
 
             // Create session for already running Microsoft Visual Studio
             var appCapabilities = new DesiredCapabilities();
             appCapabilities.SetCapability("appTopLevelWindow", visualStudioTopLevelWindowHandle);
             _driver = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
             Assert.IsNotNull(_driver);
+
+            // Set managed memory usage to not displayed this means that test will always starts on the same point
+            if (IsManagedMemoryDisplayed())
+            {
+                ToggleManagedMemoryCheckBox();
+            }
+
         }
 
         [TearDown]
-        public void TestCleanup()
+        public void AfterEachTest()
         {
-            if (_driver != null)
-            {
-                _driver.Quit();
-                _driver = null;
-            }
+            //_driver?.Quit();
+            //_driver = null;
+
+            // close "Options" window if it is not closed
+            var optionWindow = _driver.FindElementsByName("Options").FirstOrDefault();
+            optionWindow?.FindElementsByName("Save").FirstOrDefault()?.Click();
+
         }
 
         [Test]
-        public void OpenResharperOptions()
+        public void OpenReSharperOptions()
         {
-            
-            _driver.FindElementByName("ReSharper").Click(); // open ReSharper menu
-            _driver.FindElementByName("Options...").Click(); // open ReSharper menu
-            //// LegacyIAccessible.Name: "General"
-            //_driver.FindElementByName("General").Click();
-            //// LegacyIAccessible.Name: "Show managed _memory usage in status bar"
-            ////  Probably it is necessary to verify the status and check/uncheck
-            //_driver.FindElementByName("Show managed _memory usage in status bar").Click();
+            if (IsManagedMemoryDisplayed())
+            {
+                ToggleManagedMemoryCheckBox();
+            }
+        }
+
+        /// <summary>
+        /// Open ReSharper Option and toggle "Show managed memory usage in status bar" checkbox
+        /// </summary>
+        private void ToggleManagedMemoryCheckBox()
+        {
+            // Click on ReSharper menu 
+            _driver.FindElementByName("ReSharper").Click();
+            // Open ReSharper Option Dialog
+            //_driver.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]").Click();
+            // for unknown reason FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]") not able to find "Options…" menu item
+            var reSharperMenuItems = _driver.FindElementsByClassName("MenuItem");
+            reSharperMenuItems.First(p => p.Text == "Options…").Click();
+            // Find Option Window
+            var optionWindow = _driver.FindElementByName("Options");
+            Assert.IsNotNull(optionWindow);
+
+            // EMK it is not necessary to click on "Settings" but 
+            //   task description contain the following:
+            // "Важно: закладка Genral скорее всего будет выбрана по дефолту, но тест должен уметь
+            //  находить опцию, если открыта любая закладка"
+            // So switch to "Settings" to emulate the situation that some other tab is opened
+            var settings = optionWindow.FindElementByName("Settings");
+            Assert.IsNotNull(settings);
+            settings.Click();
+
+            // Locate "General" tab
+            var general = optionWindow.FindElementByName("General");
+            Assert.IsNotNull(general);
+            general.Click();
+
+            // Locate "Show managed _memory usage in status bar
+            var managedMemoryCheckBox = optionWindow.FindElementByName("Show managed _memory usage in status bar");
+            Assert.IsNotNull(managedMemoryCheckBox);
+            // toggle "Show managed _memory usage in status bar"
+            managedMemoryCheckBox.Click();
+
+            // Save & Close "Option" ReSharper Option window
+            var saveButton = optionWindow.FindElementByName("Save");
+            Assert.IsNotNull(saveButton);
+            saveButton.Click();
+        }
+
+        private bool IsManagedMemoryDisplayed()
+        {
+            return !bool.Parse(_driver.FindElementByClassName("RichTextPresenter").GetAttribute("IsOffscreen"));
         }
     }
 }
