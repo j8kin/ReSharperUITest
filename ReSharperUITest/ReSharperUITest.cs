04359c8ae4c93f6ab6a80be1c679b34f6d41769a
j8kin <emk161278@gmail.com>
2019-05-05 09:55:55 +0300

    Add DummyFailTest to obtain fail on remote host for demonstration purpose. Cleanup tests

--- ReSharperUITest/ReSharperUITest.cs
+++ ReSharperUITest/ReSharperUITest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Interactions;
@@ -16,15 +15,26 @@ namespace ReSharperUITest
 
         private WindowsDriver<WindowsElement> _driver;
         private WindowsDriver<WindowsElement> _desktopSession;
-        private int nToggle = 0;
 
-        // todo: add start Visual Studio before all TestCases
-        // todo: add close Visual Studio after all TestCases
+        [OneTimeSetUp]
+        public void BeforeAllTests()
+        {
+            // Start visual studio on remote Host
+            // path to folder with devenv.exe must be placed into PATH
+            // environment variable
+        }
+        
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            // Close visual studio on Remote Host
+            //_driver?.Quit();
+            //_driver = null;
+        }
 
         [SetUp]
         public void BeforeEachTest()
         {
-            //Console.WriteLine("SetUp");
             // Create a session for Desktop
             var desktopCapabilities = new DesiredCapabilities();
             desktopCapabilities.SetCapability("app", "Root");
@@ -34,7 +44,7 @@ namespace ReSharperUITest
             // find Microsoft Visual Studio window todo replace with partially Element Name
             var visualStudioWindow = _desktopSession.FindElementByName("ReSharperUITest - Microsoft Visual Studio ");
             var visualStudioTopLevelWindowHandle =
-                (int.Parse(visualStudioWindow.GetAttribute("NativeWindowHandle"))).ToString("x");
+                int.Parse(visualStudioWindow.GetAttribute("NativeWindowHandle")).ToString("x");
 
             // Create session for already running Microsoft Visual Studio
             var appCapabilities = new DesiredCapabilities();
@@ -42,24 +52,19 @@ namespace ReSharperUITest
             _driver = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
             Assert.IsNotNull(_driver);
 
+            // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+
             // Set managed memory usage to not displayed this means that test will always starts on the same point
             if (IsManagedMemoryDisplayed())
             {
-                //Console.WriteLine("Managed is True");
                 ToggleManagedMemoryCheckBox();
-            }
-            else
-            {
-                //Console.WriteLine("Managed is False");
             }
         }
 
         [TearDown]
         public void AfterEachTest()
         {
-            //_driver?.Quit();
-            //_driver = null;
-
             // close "Options" window if it is not closed
             var optionWindow = _driver.FindElementsByName("Options").FirstOrDefault();
             optionWindow?.FindElementsByName("Cancel").FirstOrDefault()?.Click();
@@ -67,14 +72,26 @@ namespace ReSharperUITest
         }
 
         [Test]
-        public void SwitchBetweenTabs()
+        public void ClickOnOptionMenu()
         {
             _driver.FindElementByName("ReSharper").Click();
             // Open ReSharper Option Dialog
-            //_driver.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]").Click();
+            //_driver.FindElementByXPath($"//MenuItem[contains(@Name, '_Options')]").Click();
             // for unknown reason FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]") not able to find "Options…" menu item
-            var reSharperMenuItems = _driver.FindElementsByClassName("MenuItem");
-            reSharperMenuItems.First(p => p.Text == "Options…").Click();
+            _driver.FindElementByName("ReSharper").FindElementsByXPath(".//*").First(p => p.Text == "Options…").Click();
+
+        }
+
+        [Test]
+        public void SwitchBetweenTabs()
+        {
+            _driver.FindElementByName("ReSharper").Click();
+
+            // Open ReSharper Option Dialog
+            // for unknown reason FindElementByXPath($"//MenuItem[starts-with(@Text, \"Options…\")]") not able to find "Options…" menu item
+            //  so get all children and click on child with "Options…" text
+            _driver.FindElementByName("ReSharper").FindElementsByXPath(".//*").First(p => p.Text == "Options…").Click();
+
             // Find Option Window
             var optionWindow = _driver.FindElementByName("Options");
 
@@ -94,8 +111,6 @@ namespace ReSharperUITest
         [Test]
         public void VerifyReSharperManagedMemoryOptionLogicAsync()
         {
-            Console.WriteLine("Test");
-            Console.WriteLine("Initially Managed Memory is not Displayed");
             // Initially Managed Memory is not Displayed
             Assert.False(IsManagedMemoryDisplayed());
 
@@ -103,15 +118,22 @@ namespace ReSharperUITest
             ToggleManagedMemoryCheckBox();
 
             // Verify that Managed Memory is Displayed
-            Console.WriteLine("Verify that Managed Memory is Displayed");
             Assert.True(IsManagedMemoryDisplayed());
 
             // Toggle Managed Memory Checkbox
             ToggleManagedMemoryCheckBox();
 
             // Verify that Managed Memory is not Displayed
-            Console.WriteLine("Verify that Managed Memory is Displayed");
             Assert.False(IsManagedMemoryDisplayed());
+        }
+
+        [Test]
+        public void DummyFailedTest()
+        {
+            // This test is necessary only for execution on remote host and 
+            //   demonstrate that result is fail and provide TestResult.xml 
+            //   with this fail
+            Assert.True(false);
         }
 
         /// <summary>
@@ -119,16 +141,12 @@ namespace ReSharperUITest
         /// </summary>
         private void ToggleManagedMemoryCheckBox()
         {
-            Console.WriteLine("Toggle Managed Memory Checkbox: " + nToggle);
-            nToggle++;
             // Click on ReSharper menu 
             _driver.FindElementByName("ReSharper").Click();
-            // Open ReSharper Option Dialog
 
-            //_driver.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]").Click();
-            // for unknown reason FindElementByXPath($"//MenuItem[starts-with(@Name, \"Options…\")]") not able to find "Options…" menu item
-            var reSharperMenuItems = _driver.FindElementsByClassName("MenuItem");
-            reSharperMenuItems.First(p => p.Text == "Options…").Click();
+            // Open ReSharper Option Dialog
+            _driver.FindElementByName("ReSharper").FindElementsByXPath(".//*").First(p => p.Text == "Options…").Click();
+            
             // Find Option Window
             var optionWindow = _driver.FindElementByName("Options");
 
@@ -137,22 +155,18 @@ namespace ReSharperUITest
             // "Важно: закладка Genral скорее всего будет выбрана по дефолту, но тест должен уметь
             //  находить опцию, если открыта любая закладка"
             // So switch to "Settings" to emulate the situation that some other tab is opened
-            var settings = optionWindow.FindElementByName("Settings");
-            new Actions(_driver).MoveToElement(settings).Click().Perform();
-
+            var gridElement = optionWindow.FindElementByName("Settings");
+            new Actions(_driver).MoveToElement(gridElement).Click().Perform();
 
             // Locate "General" tab
-            var general = optionWindow.FindElementByName("General");
-            new Actions(_driver).MoveToElement(general).Click().Perform();
+            gridElement = optionWindow.FindElementByName("General");
+            new Actions(_driver).MoveToElement(gridElement).Click().Perform();
 
-            // Locate "Show managed _memory usage in status bar
-            var managedMemoryCheckBox = optionWindow.FindElementByName("Show managed _memory usage in status bar");
-            // toggle "Show managed _memory usage in status bar"
-            managedMemoryCheckBox.Click();
+            // Locate "Show managed _memory usage in status bar and toggle it
+            optionWindow.FindElementByName("Show managed _memory usage in status bar").Click();
 
             // Save & Close "Option" ReSharper Option window
-            var saveButton = optionWindow.FindElementByName("Save");
-            saveButton.Click();
+            optionWindow.FindElementByName("Save").Click();
         }
 
         private bool IsManagedMemoryDisplayed()
